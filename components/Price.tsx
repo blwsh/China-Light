e358974c7d8e2be46aeed10d0933eb0b62748d03
Ben Watson <ben.watson@wearesweet.co.uk>
2021-04-27 08:37:43 +0100

    * Add new price component

---
+++ components/Price.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import globalStyles from "../styles";
+import {StyleSheet, View} from "react-native";
+import {Text} from "./index";
+import money from "../utils/money";
+
+const Price: React.FC<{price: number, discountPrice?: number|null, fontSize?: number}> = ({price, discountPrice, fontSize = 16}) => {
+  const styles = StyleSheet.create({
+    priceContainer: {flexDirection: 'row', alignItems: 'center'},
+    price: {fontSize: fontSize},
+    pricePreDiscount: {color: 'grey', fontSize: fontSize * .7, textDecorationLine: 'line-through', marginRight: 5},
+  });
+
+  return (
+    <View style={styles.priceContainer}>
+      {(typeof discountPrice !== 'undefined' && discountPrice !== null) && (
+        <Text style={styles.pricePreDiscount}>{money(price / 100)}</Text>
+      )}
+      <Text style={[globalStyles.bold, globalStyles.blue, styles.price]}>
+        {money((discountPrice || price) / 100)}
+      </Text>
+    </View>
+  )
+}
+
+export default Price;
--- styles.tsx
+++ styles.tsx
@@ -1,24 +1,12 @@
 import {StyleSheet} from "react-native";
 
 const styles = StyleSheet.create({
-  menuItem: {
-    display: 'flex',
-    backgroundColor: 'white',
-    borderRadius: 5,
-    flexWrap: 'nowrap',
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    padding: 15,
-    marginVertical: 10,
-  },
   blue: {color: '#3189c3'},
   green: {color: '#69c331'},
   gold: {color: '#ffa800'},
   grey: {color: '#a7b7b7'},
   lightGrey: {color: '#c5d0d0'},
   bold: {fontWeight: "bold"},
-  pricePreDiscount: {color: 'grey', fontSize: 12, textDecorationLine: 'line-through', marginRight: 5},
-  priceContainer: {flexDirection: 'row', alignItems: 'flex-end'}
 });
 
 export default styles;
