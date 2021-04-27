0b9413b31bbfd738bbaa494166f757aae704831a
Ben Watson <ben.watson@wearesweet.co.uk>
2021-04-27 08:39:04 +0100

    * Refactor AddMenuItemToBasket.tsx modal

--- modals/AddMenuItemToBasket.tsx
+++ modals/AddMenuItemToBasket.tsx
@@ -1,67 +1,84 @@
-import React, {useState} from "react";
-import {Image, Modal, TouchableOpacity, View} from "react-native";
-import {Button, Heading, Row, SubHeading, Text, TextInput} from "../components";
-import {BasketItem, Product} from "../types";
-import styles from "../styles";
-import money from "../utils/money";
+import React, {useCallback, useState} from "react";
+import {setWith} from "lodash";
+import {Image, KeyboardAvoidingView, Modal, ScrollView, View} from "react-native";
+import {Button} from "../components";
+import {BasketItem, Product, ProductOptionGroup} from "../types";
 import CloseButton from "./CloseButton";
 import {Quantity} from "../components/Inputs";
+import BasketHeader from "../components/Checkout/BasketHeader";
+import OptionsGroup from "../components/Checkout/OptionsGroup";
 
-type Props = {item: Product, onAddToBasket: (item: BasketItem) => void, coverImage: string} & Modal['props'];
+type Props = { item: Product, onAddToBasket: (item: BasketItem) => void, coverImage: string } & Modal['props'];
 
-const AddMenuItemToBasket: React.FC<Props> = ({item, onAddToBasket, coverImage, ...otherProps}) => {
-  const [quantity, setQuantity] = useState(1);
-  const [options, setOptions] = useState<Product[]>([]);
-
+const AddMenuItemToBasket: React.FC<Props> = ({item, onAddToBasket, onRequestClose, coverImage, ...props}) => {
   if (!item) return null;
 
-  return <Modal animationType="slide" {...otherProps}>
-    <View style={{alignItems: 'center'}}>
-      <CloseButton onPress={otherProps.onRequestClose}/>
-      <Image source={{uri: coverImage}} style={{width: '100%', height: 260, resizeMode: 'cover'}}/>
-      <View style={{width: '90%', justifyContent: 'space-between'}}>
-        <Row style={{justifyContent: 'space-between', alignItems: 'flex-start', marginTop: 20}}>
-          <Heading style={{flexShrink: 1, marginTop: 0}}>{item.name}</Heading>
+  /**
+   * State
+   */
+  const [quantity, setQuantity] = useState(1);
+  const [options, setOptions] = useState<{[key: string]: { [key: string]: Product[] }}>({});
 
-          <Row style={{flexShrink: 0, alignItems: 'flex-start'}}>
-            <Text style={[{textAlign: 'right'}, styles.pricePreDiscount]}>{money(item.price / 100)}</Text>
-            <Text style={[{
-              textAlign: 'right',
-              fontSize: 22
-            }, styles.bold, styles.blue]}>{money((item.price * .9) / 100)}</Text>
-          </Row>
-        </Row>
+  /**
+   * Handlers
+   */
+  const onSelectOption = useCallback((group: ProductOptionGroup, item: Product) => {
+    const originalOptions: Product[] = options?.[group.id]?.[item.id] || [];
 
-        <Text>{item.description}</Text>
+    if (group.limit <= 0 || originalOptions.length < group.limit) {
+      if (group.limit === 1) {
+        setOptions({...setWith(options, `[${group.id}]`, [])});
+      }
 
-        {(item.options && item.options.length > 0) &&
-          item.options?.map((group, key) => <View key={key}>
-            <SubHeading style={{marginTop: 50}}>{group.name} (Pick {group.limit})</SubHeading>
-            {group.options?.map((item, key) => (
-              <TouchableOpacity
-                key={key} accessible={true} accessibilityLabel={item.name}
-                onPress={() => setOptions([...options, item])}
-              >
-                <View style={[styles.menuItem, {padding: 5, marginVertical: 5}]}>
-                  <Text>{item.name}</Text>
-                  <View style={styles.priceContainer}>
-                    <Text style={[styles.bold, styles.blue]}>{`+ ${money((item.price * .9) / 100)}`}</Text>
-                  </View>
-                </View>
-              </TouchableOpacity>
-            ))}
-          </View>)}
+      setOptions({...setWith(options, `[${group.id}][${item.id}]`, [...originalOptions, item])});
+    }
+  }, [options]);
 
+  const onRemoveOption = useCallback((group: ProductOptionGroup, item: Product) => {
+    options[group.id][item.id] = options[group.id][item.id].splice(1);
+    setOptions({...options})
+  }, [options]);
 
-        <SubHeading style={{textAlign: 'center'}}>Quantity</SubHeading>
+  const onSubmit = useCallback(() => {
+    let optionsArray: Product[] = [];
 
-        <Quantity value={quantity} onChange={value => setQuantity(value + 1)}/>
-        <Button accessibilityLabel="Add to Basket" onPress={() => {
-          onAddToBasket({product: item, quantity, options});
-          otherProps.onRequestClose?.();
-        }}>Add to Basket</Button>
-      </View>
-    </View>
+    Object.values(options).forEach(group => Object.values(group).forEach(items => {
+      optionsArray = [...optionsArray, ...items];
+    }));
+
+    onAddToBasket({
+      product: item,
+      quantity: quantity,
+      options: optionsArray
+    });
+
+    onRequestClose?.();
+  }, [quantity, options]);
+
+  /**
+   * Render
+   */
+  return <Modal animationType="slide" {...props}>
+    <KeyboardAvoidingView behavior="padding">
+      <ScrollView contentContainerStyle={{alignItems: 'center'}}>
+        <CloseButton onPress={onRequestClose}/>
+        <Image source={{uri: coverImage}} style={{width: '100%', height: 130, resizeMode: 'cover'}}/>
+
+        <View style={{width: '90%', justifyContent: 'space-between'}}>
+          <BasketHeader item={item}/>
+
+          {(item.options && item.options.length > 0) && item.options?.map((group, key) => (
+            <OptionsGroup group={group} selectedOptionsMap={options} onSelectOption={onSelectOption} onRemoveOption={onRemoveOption}/>
+          ))}
+
+          <Quantity style={{marginVertical: 20}} value={quantity} max={10} onChange={value => setQuantity(value + 1)}/>
+
+          <Button size="large" style={{marginBottom: 10}} onPress={onSubmit}>Add to Basket</Button>
+
+          <Button size="regular" color="transparent" textColor="red" style={{borderWidth: 1, borderColor: 'red', marginBottom: 10}} onPress={() => onRequestClose?.()}>Cancel</Button>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   </Modal>;
 }
 
