e22149b7161b07c66b7ca93e32165aeb41a8440f
Ben Watson <ben.watson@wearesweet.co.uk>
2021-04-27 08:38:49 +0100

    * Add price component to components barrel

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,7 @@
 import Button from './Button'
 import Heading from './Heading'
 import Loading from './Loading'
+import Price from './Price'
 import Rating from './Rating'
 import * as Restaurant from './Restaurant';
 import Row from './Row'
@@ -8,4 +9,4 @@ import SubHeading from './SubHeading'
 import Text from './Text'
 import TextInput from './TextInput'
 
-export {Button, Heading, Loading, Rating, Restaurant, Row, SubHeading, Text, TextInput}
+export {Button, Heading, Loading, Price, Rating, Restaurant, Row, SubHeading, Text, TextInput}
