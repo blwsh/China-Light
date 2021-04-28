e94b28be7a4372e4a83c7f9aa7e79ed3c916a4ce
Ben Watson <ben.watson@wearesweet.co.uk>
2021-04-28 02:08:59 +0100

    * Add calculateDelivery util

---
+++ utils/calculateDelivery.tsx
@@ -0,0 +1,13 @@
+import {Address} from "../types";
+
+const restaurantAddress: Address = {
+  city: "Huddersfield", country: "United Kingdom", line_1: "52 Market St", post_code: "HD3 4HT",
+  geo: {lat: 53.6546552, lng: -1.8332872}
+}
+
+const deliveryAddress: Address = {
+  city: "Huddersfield", country: "United Kingdom", line_1: "221 New Hey Road", post_code: "HD3 4GD",
+  geo: {lat: 53.6407513, lng: -1.8286269}
+}
+
+
